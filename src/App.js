4867478e67669e13529182932cbde1dd25841bbe
leandross86 <leandrobko@gmail.com>
2020-09-12 14:25:31 -0300

    [INICIO] primeiro passos

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import Navigation from './Navigation'
+
+export default function App() {
+  return (
+    <Navigation />
+    
+  );
+}
---
+++ src/Navigation.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import Home from './view/Home'
+
+const Stack = createStackNavigator()
+
+export default function Navigation(){
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name='Home'
+          component={Home}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
